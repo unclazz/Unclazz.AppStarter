c4830ea62d845ad5561c823e02ee876cb8961916
mizuky fujitani <mizuki.fujitani@gmail.com>
2017-07-06 00:13:20 +0900

    DefaultAppStatisticsのテストケースを追加

---
+++ Test.Unclazz.AppStarter/DefaultAppStatisticsTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unclazz.AppStarter;
+
+namespace Test.Unclazz.AppStarter
+{
+    [TestFixture]
+    public class DefaultAppStatisticsTest
+    {
+        [TestCase(@"C:\path\to\TestAssembly.dll", @"C:\path\to\TestAssembly.dll", ExpectedResult = false)]
+        [TestCase(@"C:\path\to\TESTAS~1.dll", @"C:\path\to\TestAssembly.dll", ExpectedResult = true)]
+        public bool ShortNameUsed_CheckIfShortFileNameIsUsed(string arg0, string arg1)
+        {
+            // Arrange
+            EntryAssemblyUtility.AssemblyLocation = arg0;
+            EntryAssemblyUtility.AssemblyFullPath = arg1;
+
+            // Act
+            // Assert
+            return new DefaultAppStatistics().ShortNameUsed;
+        }
+    }
+}
